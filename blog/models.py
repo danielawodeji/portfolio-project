52f8b5b24522a2ac4d85f97d2ca4c76096bb26c5
Daniel Awodeji <danielproawodeji@gmail.com>
2020-11-19 17:57:04 +0100

    good

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,6 @@
 from django.db import models
-
+class Blog(models.Model):
+    title = models.CharField(max_length=400)
+    pub_date=models.DateTimeField()
+    body=models.CharField(max_length=100000)
+    Image=models.ImageField(upload_to='images/')
