eb030fc9804678e0571eca7f273d888a8b4d1527
Daniel Awodeji <danielproawodeji@gmail.com>
2020-11-19 17:11:33 +0100

    database postgresql

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolioprojectdb',
+        'USER':'postgres',
+        'PASSWORD':'sewa',
+        'HOST':'localhost',
+        'PORT':'5432',
+
     }
 }
 
